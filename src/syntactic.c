200d1e3fa2ee78c66a753a25f6471920113a5e46
xmatya03 <xmatya03@stud.fit.vutbr.cz>
2012-11-17 21:26:44 +0100

    Vytvoreni souboru pro syntaktickou analyzu

---
+++ src/syntactic.c
@@ -0,0 +1,21 @@
+/**
+ * Predmet:   IFJ / IAL
+ * Projekt:   Implementace interpretu imperativniho jazyka IFJ12
+ * Varianta:  a/1/I
+ * Soubor:    syntactic.c
+ * Popis:     Syntakticka analyza
+ * Datum:     17.11.2012
+ * Autori:    Frantisek Kolacek   <xkolac12@stud.fit.vutbr.cz>
+ *            Petr Matyas         <xmatya03@stud.fit.vutbr.cz>
+ *            Veronika Necasova   <xnecas21@stud.fit.vutbr.cz>
+ *            Michaela Muzikarova <xmuzik04@stud.fit.vutbr.cz>
+ *            Dalibor Skacel      <xskace11@stud.fit.vutbr.cz>
+ */
+
+#include "syntactic.h"
+
+/**
+ * @info      Something
+ * @param   parametry
+ * @return  Whatever
+ */
