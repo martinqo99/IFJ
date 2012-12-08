22291dad538226cd8dd05e40d6e9181cbff83b0b
xmatya03 <xmatya03@stud.fit.vutbr.cz>
2012-12-08 21:12:04 +0100

    zacatek interpretu

--- src/interpret.c
+++ src/interpret.c
@@ -21,5 +21,64 @@
  */
 E_CODE interpret (tList instr)
 {
+  tListItem *help = instr.first;
+  E_CODE err = ERROR_OK;
+  tStack *stack;
+  err = stackInit(stack);
 
+  while (help != NULL && err == ERROR_OK) {
+    switch ((tInstr* help->data)->type) {
+      case I_RETURN:
+        break;
+
+      case I_POP:
+        help->data->dest = stackPop(stack);
+        //mozna jeste neco dalsiho :)
+        break;
+
+      case I_PUSH:
+        err = stackPush(stack, (tInstr* help->data)->src1);
+        // ze by neco vic?
+        break;
+
+      case I_SEMPTY:
+        err = stackDispose(stack);
+        break;
+
+      case I_MOV:
+        help->data->dest = help->data->src1;
+        break;
+
+      case I_SET:
+        break;
+
+      case I_ADD:
+        if (help->data->src1->type == DT_NUMBER && help->data->src1->type == DT_NUMBER)
+          help->data->dest->data->data.dData = help->data->src1->data->data.dData + help->data->src2->data->data.dData;
+        else if (help->data->src1->type == DT_STRING && help->data->src1->type == DT_STRING) {
+          int temp = help->data->src1->data->data.sData.len + help->data->src2->data->data.sData.len;
+          help->data->dest->data->data.sData.data = mmuMalloc(sizeof(temp*char));
+          if (help->data->dest->data->data.sData.data == NULL) return ERROR_COMPILATOR;
+          help->data->dest->data->data.sData.data = strcat (help->data->src1->data->data.sData.data, help->data->src2->data->data.sData.data);
+        }
+        else if (help->data->src1->type == DT_NUMBER && help->data->src1->type == DT_STRING) {
+          // tady nevim
+        }
+        else if (help->data->src1->type == DT_STRING && help->data->src1->type == DT_NUMBER) {
+          // tady nevim
+        }
+        else return ERROR_SEMANTIC;
+        break;
+
+      case I_SUB:
+        if (help->data->src1->type == DT_NUMBER && help->data->src1->type == DT_NUMBER)
+          help->data->dest->data->data.dData = help->data->src1->data->data.dData - help->data->src2->data->data.dData;
+        else return ERROR_SEMANTIC;
+        break;
+
+      case I_MUL:
+
+    }
+    help = help->next;
+  }
 }
