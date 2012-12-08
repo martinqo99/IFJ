cd5e6c90f1fdade7ce663cd150e0c3beb61496aa
xmatya03 <xmatya03@stud.fit.vutbr.cz>
2012-12-08 16:44:04 +0100

    Upraveny komentare quickosrtu

--- src/quicksort.h
+++ src/quicksort.h
@@ -2,9 +2,9 @@
  * Predmet:   IFJ / IAL
  * Projekt:   Implementace interpretu imperativniho jazyka IFJ12
  * Varianta:  a/1/I
- * Soubor:    strings.h
- * Popis:     Tento soubor definuje co je to string
- * Datum:     17.10.2012
+ * Soubor:    quicksort.h
+ * Popis:     Hlavickovy soubor pro quicksort
+ * Datum:     8.12.2012
  * Autori:    Frantisek Kolacek   <xkolac12@stud.fit.vutbr.cz>,
  *            Petr Matyas         <xmatya03@stud.fit.vutbr.cz>,
  *            Veronika Necasova   <xnecas21@stud.fit.vutbr.cz>,
